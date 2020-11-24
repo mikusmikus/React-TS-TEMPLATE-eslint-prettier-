c535ddc5a70e0c8b01b4027f2f2b10d65129f207
Mikus <mikusmeikulis@gmail.com>
2020-11-25 01:43:36 +0200

    add

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import 'flexboxgrid';
+
+const App = () => {
+  return (
+    <div>
+      <div className="container">
+        <h1>hello World!</h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
